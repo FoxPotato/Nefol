5a54d4b8c8d84be3af62f4042381da23780ca87c
FoxPotato <foxpotato5@gmail.com>
2016-05-05 05:27:59 +0200

    stars randomly rotate now, just for testing

---
+++ Assets/drawLine.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class drawLine : MonoBehaviour {
+
+	private LineRenderer lineRenderer;
+	private float counter;
+	private float dist;
+
+	public GameObject centerCube;
+	private rotateondrag centerScript;
+
+	public Transform[] stars;
+
+	public float lineWidth = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		lineRenderer = GetComponent<LineRenderer> ();
+		lineRenderer.SetWidth (lineWidth, lineWidth);
+		lineRenderer.SetVertexCount (stars.Length);
+
+		centerCube = GameObject.Find ("Cube");
+		centerScript = centerCube.GetComponent<rotateondrag> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (!GameObject.Find ("Cube").GetComponent<rotateondrag> ().isCorrect) {
+			lineRenderer.SetVertexCount (0);	
+			return;
+		} else
+			lineRenderer.SetVertexCount (stars.Length);
+
+		for(int i = 0; i < stars.Length; i++)
+			lineRenderer.SetPosition (i, stars[i].position);
+	}
+}
--- Assets/rotateondrag.cs
+++ Assets/rotateondrag.cs
@@ -3,21 +3,26 @@ using System.Collections;
 
 public class rotateondrag : MonoBehaviour {
 
-	float 	lastX 		= 0.0f;
-	float 	difX 		= 0.5f;
-	int 	directionX 	= 0;
+	public bool isCorrect = false;
 
-	float 	lastY 		= 0.0f;
-	float 	difY 		= 0.5f;
-	int 	directionY 	= 0;
+	float 			lastX 		= 0.0f;
+	float 			difX 		= 0.5f;
 
-	float	extraSteps 	= 0.5f;
+	float 			lastY 		= 0.0f;
+	float 			difY 		= 0.5f;
+
+	float			extraSteps 	= 0.1f;
+
+	public float	margin 		= 4f;
 
 	Quaternion startRotation;
 
 	// Use this for initialization
 	void Start () {
 		startRotation = transform.rotation;
+
+		System.Random rand = new System.Random ();
+		transform.Rotate (rand.Next (50, 359), rand.Next (50, 359), rand.Next (50, 359));
 	}
 
 	// Update is called once per frame
@@ -25,61 +30,47 @@ public class rotateondrag : MonoBehaviour {
 
 		if (Input.GetKeyDown (KeyCode.Escape)) 
 		{
-			directionX = 0;
-			directionY = 0;
-			transform.rotation = startRotation;
+			System.Random rand = new System.Random ();
+			transform.Rotate (rand.Next (50, 359), rand.Next (50, 359), rand.Next (50, 359));
 		}
 
-		if (Input.GetMouseButtonDown (0)) 
+		if (Input.GetMouseButtonDown (0) && !isCorrect) 
 		{
 			difX = 0.0f;
-			difY = 0.0f;
+			difY = 0.0f; 
 		}
-		else if (Input.GetMouseButton (0)) {
+		else if (Input.GetMouseButton (0) && !isCorrect) {
 			difX = Mathf.Abs (lastX - Input.GetAxis ("Mouse X"));
 
-			if (lastX < Input.GetAxis ("Mouse X")) {
-				directionX = -1;
+			if (lastX < Input.GetAxis ("Mouse X")) 
 				transform.Rotate(0, -difX - extraSteps, 0, Space.World);
-			}
 
-			if (lastX > Input.GetAxis ("Mouse X")) {
-				directionX = 1;
+			if (lastX > Input.GetAxis ("Mouse X")) 
 				transform.Rotate(0, difX + extraSteps, 0, Space.World);
-			}
 
 			lastX = -Input.GetAxis ("Mouse X");
 
-
-
 			difY = Mathf.Abs (lastY - Input.GetAxis ("Mouse Y"));
 
-			if (lastY < Input.GetAxis ("Mouse Y")) {
-				directionY = -1;
+			if (lastY < Input.GetAxis ("Mouse Y"))
 				transform.Rotate(difY + extraSteps, 0, 0, Space.World);
-			}
-			if (lastY > Input.GetAxis ("Mouse Y")) {
-				directionY = 1;
+			
+			if (lastY > Input.GetAxis ("Mouse Y"))
 				transform.Rotate(-difY - extraSteps, 0, 0, Space.World);
-			}
-
+			
 			lastY = -Input.GetAxis ("Mouse Y");
 		} 
 		else 
 		{
-//			if (f_difX > 0.5f)
-//				f_difX -= 0.05f;	
-//			if (f_difX < 0.5f)
-//				f_difX += 0.05f;
-//		
-//			transform.Rotate(0, f_difX * i_directionX, 0, Space.World);
-//
-//			if (f_difY > 0.5f)
-//				f_difY -= 0.05f;	
-//			if (f_difY < 0.5f)
-//				f_difY += 0.05f;
-//			
-//			transform.Rotate(f_difY * i_directionY, 0, 0, Space.World);
+			
+			if ((transform.rotation.eulerAngles.x < margin || transform.rotation.eulerAngles.x > (360f - margin)) &&
+			   (transform.rotation.eulerAngles.y < margin || transform.rotation.eulerAngles.y > (360f - margin))) {
+				isCorrect = true;
+				transform.rotation = Quaternion.Slerp(transform.rotation, startRotation, Time.time * 0.0001f);
+			}
+			else
+				isCorrect = false;
+
 		}
 
 	}
